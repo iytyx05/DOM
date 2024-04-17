cc8a1650335c3dc4bcad9f2d747654f24020955e
Islam Musabekov <islamchik3039@gmail.com>
2024-04-17 16:23:06 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// Задание №1
+//  Всем <h3> поставьте текст '!!!'.
+// let text = document.querySelectorAll("h3");
+// text.forEach((elem) => {
+//     elem.append("!!!")
+// })
+
+// Задание №2
+//  Всем <h3> сделайте текст зеленого цвета.
+// let text = document.querySelectorAll("h3");
+// text.forEach((elem) => {
+//     elem.style.color = "green"
+// })
+
+// Задание №3
+//  Создайте маркированный список,
+// При помощи цикла добавьте в маркированный
+// список 30 li - элементов с текстом:
+// “1 - овечка”, “2 - овечка”,
+// “3 - овечка”,
+// и так до “30-овечка”;
+// let ul = document.createElement("li")
+// console.log(ul)
+// let body = document.querySelector("body")
+// for (let i = 1; i <= 30; i++){
+//     let li = document.createElement("li")
+//     li.innerText = `${i} - овечек`
+//     ul.append(li)
+// }
+// console.log(ul)
+// body.append(ul)
+
+// Задание №4
+// В css создайте класс .active в
+// котором пропишите цвет текста blue.
+// Добавьте всем span элементам класс active
+// через document.getElementsByTagName.
+
+// let span = document.getElementsByTagName("span")
+// console.log(span)
+// for (i of span){
+//     i.className = "active"
+// }
+// console.log(i)
